ed1f42e2f6efc2289b59f03332c5823f6576d05d
MikoJIa <Kolya_popov_86@tut.by>
2023-11-19 11:41:41 +0300

    create favorite_task view and templates

--- project_app/urls.py
+++ project_app/urls.py
@@ -4,5 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('create_task/', add_task, name='create_task')
+    path('create-task/', add_task, name='create_task'),
+    path('favorite-task/', favorite_task, name='favorite_task'),
+    path('add-to-favorite/<int:task_id>/', add_to_favorite, name='add_to_favorite')
 ]
--- project_app/views.py
+++ project_app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from project_app.forms import CreateTaskForm
-from project_app.models import Task
+from project_app.models import Task, FavoriteTask
 
 
 def index(request):
@@ -26,3 +26,18 @@ def add_task(request):
     return render(request, 'create_task.html', content)
 
 
+def favorite_task(request):
+    favorite_task = FavoriteTask.objects.filter(user=request.user)
+    context = {
+        'favorite_task': [task.task for task in favorite_task]
+    }
+    return render(request, 'favorite_task.html', context)
+
+
+def add_to_favorite(request, task_id):
+    if request.method == 'POST':
+        task = get_object_or_404(Task, pk=task_id)
+        FavoriteTask.objects.create(user=request.user, task=task)
+        return redirect('favorite_task')
+    else:
+        return redirect('index')
