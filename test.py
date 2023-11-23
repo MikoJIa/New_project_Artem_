17d769247cba2bf6253fc920179d9f2c7fe39c5a
MikoJIa <Kolya_popov_86@tut.by>
2023-11-23 18:18:21 +0300

    add task_delete

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-import string
-
-
-def alphabet_position(text):
-    alpha = list(string.ascii_lowercase)
-    d = {item: num + 1 for num, item in enumerate(alpha)}
-    new_string = ''
-    for item in text.lower():
-        if item in d:
-            new_string += str(d[item]) + ' '
-    return new_string[:-1]
-
-
-print(alphabet_position("The sunset sets at twelve o' clock."))
+print(-5 % 3, -5 // 3)
